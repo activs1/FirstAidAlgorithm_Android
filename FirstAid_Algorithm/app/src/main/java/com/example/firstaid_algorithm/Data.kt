04f58dcf831b18d6d7a902a5b7d0ea69870fd3b4
JL <jlasek98@gmail.com>
2020-05-05 16:56:33 +0200

    x

---
+++ FirstAid_Algorithm/app/src/main/java/com/example/firstaid_algorithm/Data.kt
@@ -0,0 +1,32 @@
+package com.example.firstaid_algorithm
+
+class Data {
+    var questions = arrayOf(
+    "Czy jesteś bezpieczny?",
+    "Czy poszkodowany jest przytomny?",
+    "Czy widzisz poważny upływ krwi?",
+    "Czy sprawa jest poważna?",
+    "Czy poszkodowany oddycha?",
+    "Czy wiesz co się stało?",
+    "Czy oddech powrócił?",
+    "Czy podejrzewasz uszkodzenie kręgosłupa?"
+    )
+
+    var tips = arrayOf(
+        "Nie podchodź!",
+        "Załóż rękawiczki i zawołaj głośno poszkodowanego",
+        "Zatamuj krwotok i zbierz wywiad",
+        "Zbierz wywiad",
+        "Upewnij się, że poszkodowany jest zdrowy i poradzi sobie sam",
+        "Zadzwoń na 999 lub 112. ",
+        "Znajdź osobę do pomocy",
+        "Zatamuj krwotok",
+        "Oceń funkcje życiowe",
+        "Natychmiastowa resuscytacja!!!",
+        "Pozycja czterokończynowa",
+        "Zbadaj poszkodowanego, poszukaj obrażeń i sprawdzaj oddech",
+        "Pozycja bezpieczna (boczna ustalona), sprawdzaj oddech",
+        "Pozycja zastana, sprawdzaj oddech",
+        "Czekaj na przyjazd karetki"
+    )
+}
